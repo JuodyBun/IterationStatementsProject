9c7ba762b0a63129ab3730b46cf8d3c6c8cbc7d5
Juody Bun <bjuody@gmail.com>
2020-01-21 12:56:14 -0600

    Finished Project

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -10,32 +10,43 @@ namespace IterationStatements
             //TODO
 
             //Create a List called "numbers"
+            List<int> numbers = new List<int>();
             //Create a variable of type int with an initializer of 0
-
+            int index = 0;
 
 
             // Create a do-while loop
+            do
             {
                 // Increment your variable by 1
+                index++;
                 // Then add your variable to "numbers"
-            }
+                numbers.Add(index);
+            } while (index < 100);
             // While your variable is less than 100
-
-
 
             // Create a while loop
             // While your variable is less than 200
+            while (index < 200)
             {
                 // Increment your variable by 1
+                index++;
                 // Then add your variable to "numbers"
+                numbers.Add(index);
             }
 
             Console.WriteLine("Increase:");
 
             // Create a foreach loop
-            // Write your variable to the console
+            foreach(int number in numbers)
+            // Write your variable to the console 
+            {
+                Console.WriteLine(number);
+            }
 
-            Console.WriteLine("");
+            Console.WriteLine();
+            Console.WriteLine("********************");
+            Console.WriteLine();
             Console.WriteLine("Decrease:");
 
             // Create a for loop
@@ -43,9 +54,13 @@ namespace IterationStatements
             // in your conditional, as long as i is less than or equal to the length of "numbers"
             // and as long as i is greater than or equal to 0
             // Decrement i by 1
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
             {
                 // Write to the console "numbers" at index i
+                Console.WriteLine(numbers[i]);
             }
+
+            Console.ReadLine();
         }
     }
 }
